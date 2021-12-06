bb9e3068fd5beaaa661fb1ffd7c5f3d3f51865ef
Daniel <daniel.jones424@gmail.com>
2021-12-05 21:49:23 -0500

    commit tree eb373abf4dd97bc44575bf43bbb347cd500ff8a3

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,22 @@
 class ArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
+
   def index
     articles = Article.all.includes(:user).order(created_at: :desc)
     render json: articles, each_serializer: ArticleListSerializer
   end
 
   def show
+    session[:page_views] ||= 0
     article = Article.find(params[:id])
-    render json: article
+    session[:page_views] = session[:page_views] + 1
+    if session[:page_views] < 3
+      render json: article
+    elsif session[:page_views] >= 3
+      render json: {error:  "Maximum pageview limit reached"}, status: 401
+    end
+
   end
 
   private
